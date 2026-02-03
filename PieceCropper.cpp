5fff35f1be90cb386e4341f2da7a88e7feef78d9
Mostafa Elfaggal <mostafaelfaggal@gmail.com>
2026-02-03 16:06:00 +0200

    blue bottom square highlighting in piece cropper

--- PieceCropper.cpp
+++ PieceCropper.cpp
@@ -256,7 +256,7 @@ std::vector<cv::Mat> extractWarpedSquares(
 
                 for (int y = y0; y < y1; ++y) {
                     for (int x = x0; x < x1; ++x) {
-                        warped.at<Vec3b>(y, x) = Vec3b(255, 0, 0); // Blue in BGR
+                        warped.at<Vec3b>(y, x) = Vec3b(0, 0, 255); // Blue in BGR
                     }
                 }
             }
